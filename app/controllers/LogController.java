d33e1a089cd84903e0a36598b1e3da9f0b5850ed
Alina Avery <amvavery@gmail.com>
2016-04-06 09:38:05 -0400

    remove trace

--- app/controllers/LogController.java
+++ app/controllers/LogController.java
@@ -23,43 +23,43 @@ public class LogController extends Controller {
    * Accept a batch of log entries from a machine.
    */
   @BodyParser.Of(value=BodyParser.Raw.class, maxLength=11000000)
-  public static Result log(final String machine) throws Exception {
-    try {
-      // Throw an exception if machine ID is invalid.
-      Database.getMachine(machine);
+    public static Result log(final String machine) throws Exception {
+      try {
+        // Throw an exception if machine ID is invalid.
+        Database.getMachine(machine);
 
-      // Log a "log" event.
-      Database.logEvent(machine, "log");
+        // Log a "log" event.
+        Database.logEvent(machine, "log");
 
-      if (request().body().isMaxSizeExceeded()) {
-        throw new Exception("max size exceeded");
-      }
-
-      // Access the file that Play used to buffer the content.
-      final File file = request().body().asRaw().asFile();
-
-      // Process the file in the background.
-      new Thread(new Runnable() {
-        @Override
-        public void run() {
-          // Parse log entries, line by line.
-          parseLogEntries(file, machine);
-
-          // Move the whole file to the /tmp folder for upload to S3.
-          // (The directory name should be a configuration option...)
-          if (!file.renameTo(new File("/tmp", logFileName(machine)))) {
-            logger.warn("/log/" + machine + " failed to move"); 
-          }
+        if (request().body().isMaxSizeExceeded()) {
+          throw new Exception("max size exceeded");
         }
-      }).start();
 
-      return ok();
+        // Access the file that Play used to buffer the content.
+        final File file = request().body().asRaw().asFile();
+
+        // Process the file in the background.
+        new Thread(new Runnable() {
+          @Override
+          public void run() {
+            // Parse log entries, line by line.
+            parseLogEntries(file, machine);
+
+            // Move the whole file to the /tmp folder for upload to S3.
+            // (The directory name should be a configuration option...)
+            if (!file.renameTo(new File("/tmp", logFileName(machine)))) {
+              logger.warn("/log/" + machine + " failed to move");
+            }
+          }
+        }).start();
+
+        return ok();
+      }
+      catch (Exception e) {
+        logger.error("/log/" + machine + " error", e);
+        return badRequest();
+      }
     }
-    catch (Exception e) {
-      logger.error("/log/" + machine + " error", e); 
-      return badRequest();
-    }
-  }
 
   private static void parseLogEntries(File file, String machine) {
     try {
@@ -72,39 +72,38 @@ public class LogController extends Controller {
             try {
               JsonNode json = Json.parse(line);
               if (json.get("priority") != null) {
-            	  String time = json.get("time").asText();
-            	  SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM yyyy HH:mm:ss,SSS");
-            	  Date parsedDate = dateFormat.parse(time);
-            	  Timestamp timestamp = new java.sql.Timestamp(parsedDate.getTime());
-            	  if ("INFO".equals(json.get("priority").textValue())) {
-            		  //write to events table
-            		  JsonNode jn = json.get("message");
-            		  System.out.println(jn);
-            		  String machine_id = jn.get("machine_id").asText();
-            		  String event_type = jn.get("event").asText();
-            		  String product_sku = jn.get("product_id").asText();
-            		  Database.logEvent(machine_id, event_type, product_sku, timestamp);
-            	  } else if ("DEBUG".equals(json.get("priority").textValue())) {
-            		  //write to machine_log table
-            		  JsonNode jn = json.get("message");
-            		  int machine_id = jn.get("machine_id").asInt();
-            		  int traffic = jn.get("traffic").asInt();
-            		  int jammed = jn.get("jammed").asInt();
-            		  try {
-            		      Database.logMachineStatus(machine_id, jammed, traffic, timestamp);
-            		  } catch (Exception e) {
-            			  logger.error("SQLException: " + e.toString());
-            		  }
-            	  } else if ("ERROR".equals(json.get("priority").textValue())) {
-            		  //write to file for later 
-            	  } else if ("WARN".equals(json.get("priority").textValue())) {
-            		  //email team to alert
-            		  Email.emailLogError(json.get("message").toString());
-            	  } else {
-            		  //unknown or other priority - TRACE
-            	  }
+                String time = json.get("time").asText();
+                SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM yyyy HH:mm:ss,SSS");
+                Date parsedDate = dateFormat.parse(time);
+                Timestamp timestamp = new java.sql.Timestamp(parsedDate.getTime());
+                if ("INFO".equals(json.get("priority").textValue())) {
+                  //write to events table
+                  JsonNode jn = json.get("message");
+                  String machine_id = jn.get("machine_id").asText();
+                  String event_type = jn.get("event").asText();
+                  String product_sku = jn.get("product_id").asText();
+                  Database.logEvent(machine_id, event_type, product_sku, timestamp);
+                } else if ("DEBUG".equals(json.get("priority").textValue())) {
+                  //write to machine_log table
+                  JsonNode jn = json.get("message");
+                  int machine_id = jn.get("machine_id").asInt();
+                  int traffic = jn.get("traffic").asInt();
+                  int jammed = jn.get("jammed").asInt();
+                  try {
+                    Database.logMachineStatus(machine_id, jammed, traffic, timestamp);
+                  } catch (Exception e) {
+                    logger.error("SQLException: " + e.toString());
+                  }
+                } else if ("ERROR".equals(json.get("priority").textValue())) {
+                  //write to file for later
+                } else if ("WARN".equals(json.get("priority").textValue())) {
+                  //email team to alert
+                  Email.emailLogError(json.get("message").toString());
+                } else {
+                  //unknown or other priority - TRACE
+                }
               } else {
-            	  //missing priority from json object
+                //missing priority from json object
               }
             }
             catch (Exception ignore) {
@@ -117,7 +116,7 @@ public class LogController extends Controller {
       }
     }
     catch (IOException e) {
-      logger.error("/log/" + machine + " error", e); 
+      logger.error("/log/" + machine + " error", e);
     }
   }
 
